b618c3f8cb49ed946bd571cad2dec157244804e6
Lórien <199116911+lorien349@users.noreply.github.com>
2025-04-02 16:49:34 +0200

    Add the rest of the game's functionalities

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice () {
     let randomNum = Math.random()
     let computerChoice = undefined
@@ -13,4 +17,60 @@ function getComputerChoice () {
     return computerChoice
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice () {
+    let option = prompt("Insert rock, paper, or scissors").toLowerCase()
+    return option
+}
+
+function playGame () {
+    function playRound (computerChoice = getComputerChoice(), playerChoice = getPlayerChoice()) {
+        let winner = undefined
+        let draw = false
+    
+        if (computerChoice == playerChoice) {
+            draw = true
+        } else {
+            if (computerChoice == "rock") {
+                if (playerChoice == "paper") {
+                    winner = "player"
+                } else if (playerChoice == "scissors") {
+                    winner = "computer"
+                }
+            } else if (computerChoice == "paper") {
+                if (playerChoice == "rock") {
+                    winner = "computer"
+                } else if (playerChoice == "scissors") {
+                    winner = "player"
+                }
+            } else if (computerChoice == "scissors") {
+                if (playerChoice == "rock") {
+                    winner = "player"
+                } else if (playerChoice = "paper") {
+                    winner = "computer"
+                }
+            }
+        }
+    
+        if (draw == true) {
+            return "Draw"
+        } else {
+            if (winner == "computer") {
+                ++computerScore
+                return "You lost"
+            } else if (winner == "player") {
+                ++playerScore
+                return "You win!!"
+            }
+        }
+    }
+
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+}
+
+
+
+playGame()
