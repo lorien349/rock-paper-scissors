2491caea11257c19444aa3f22ce6595f4f257adf
Lórien <199116911+lorien349@users.noreply.github.com>
2025-04-18 13:59:02 +0200

    Add part of game's logic and functionality
    Refactory script.js to apply the logic to the UI, and remake it

--- script.js
+++ script.js
@@ -1,76 +1,117 @@
-let playerScore = 0;
-let computerScore = 0;
+// Game's variables
+let playerChoice = undefined;
+let computerChoice = undefined;
+let winner = undefined;
+let isFirstRound = true;
+
+// Colors
+const LIME_COLOR = "#3DFE84";
+const DEFAULT_GRAY_COLOR = "#c5c5c5";
+const DEFAULT_BORDER_GRAY_COLOR = "#929292";
+const GREEN_COLOR = "#28a745";
+const RED_COLOR = "#dc3545";
+const YELLOW_COLOR = "#ffc107";
+
+// DOM nodes
+const allOptionsContainer = document.querySelector("#all-options-container");
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
+const playButton = document.querySelector("#play-button");
+const wonLostText = document.querySelector("#won-lost-text");
 
 
-function getComputerChoice () {
-    let randomNum = Math.random()
-    let computerChoice = undefined
+
+// Functions
+assignPlayerChoice = function(button) {
+    const id = button.getAttribute("id");
+
+    switch(id) {
+        case "rock-button":
+            playerChoice = "rock";
+            break;
+        case "paper-button":
+            playerChoice = "paper";
+            break;
+        case "scissors-button":
+            playerChoice = "scissors";
+            break;
+    }
+
+    switch(playerChoice) {
+        case "rock":
+            rockButton.style.backgroundColor = LIME_COLOR;
+            rockButton.style.border = `3px, solid, ${LIME_COLOR}`;
+            rockButton.textContent = "SELECTED";
+
+            paperButton.style.backgroundColor = DEFAULT_GRAY_COLOR;
+            paperButton.style.border = `3px, solid, ${DEFAULT_BORDER_GRAY_COLOR}`;
+            paperButton.textContent = "SELECT";
+
+            scissorsButton.style.backgroundColor = DEFAULT_GRAY_COLOR;
+            scissorsButton.style.border = `3px, solid, ${DEFAULT_BORDER_GRAY_COLOR}`;
+            scissorsButton.textContent = "SELECT";
+
+            break;
+
+        case "paper":
+            paperButton.style.backgroundColor = LIME_COLOR;
+            paperButton.style.border = `3px, solid, ${LIME_COLOR}`;
+            paperButton.textContent = "SELECTED";
+
+            rockButton.style.backgroundColor = DEFAULT_GRAY_COLOR;
+            rockButton.style.border = `3px, solid, ${DEFAULT_BORDER_GRAY_COLOR}`;
+            rockButton.textContent = "SELECT";
+
+            scissorsButton.style.backgroundColor = DEFAULT_GRAY_COLOR;
+            scissorsButton.style.border = `3px, solid, ${DEFAULT_BORDER_GRAY_COLOR}`;
+            scissorsButton.textContent = "SELECT";
+
+            break;
+        
+        case "scissors":
+            scissorsButton.style.backgroundColor = LIME_COLOR;
+            scissorsButton.style.border = `3px, solid, ${LIME_COLOR}`;
+            scissorsButton.textContent = "SELECTED";
+
+            paperButton.style.backgroundColor = DEFAULT_GRAY_COLOR;
+            paperButton.style.border = `3px, solid, ${DEFAULT_BORDER_GRAY_COLOR}`;
+            paperButton.textContent = "SELECT";
+
+            rockButton.style.backgroundColor = DEFAULT_GRAY_COLOR;
+            rockButton.style.border = `3px, solid, ${DEFAULT_BORDER_GRAY_COLOR}`;
+            rockButton.textContent = "SELECT";
+
+            break;
+    }
+
+    console.log(playerChoice);
+};
+
+assignComputerChoice = function() {
+    let randomNum = Math.random();
 
     if (randomNum >= 0 && randomNum <= 0.3) {
-        computerChoice = "rock"
+        computerChoice = "rock";
     } else if (randomNum > 0.3 && randomNum <= 0.6) {
-        computerChoice = "paper"
+        computerChoice = "paper";
     } else if (randomNum > 0.6 && randomNum <= 1) {
-        computerChoice = "scissors"
+        computerChoice = "scissors";
     }
 
-    return computerChoice
-}
+    console.log(computerChoice);
+};
 
-function getPlayerChoice () {
-    let option = prompt("Insert rock, paper, or scissors").toLowerCase()
-    return option
-}
-
-function playGame () {
-    function playRound (computerChoice = getComputerChoice(), playerChoice = getPlayerChoice()) {
-        let winner = undefined
-        let draw = false
+playRound = function(playerChoice, computerChoice = assignComputerChoice()) {
     
-        if (computerChoice == playerChoice) {
-            draw = true
-        } else {
-            if (computerChoice == "rock") {
-                if (playerChoice == "paper") {
-                    winner = "player"
-                } else if (playerChoice == "scissors") {
-                    winner = "computer"
-                }
-            } else if (computerChoice == "paper") {
-                if (playerChoice == "rock") {
-                    winner = "computer"
-                } else if (playerChoice == "scissors") {
-                    winner = "player"
-                }
-            } else if (computerChoice == "scissors") {
-                if (playerChoice == "rock") {
-                    winner = "player"
-                } else if (playerChoice = "paper") {
-                    winner = "computer"
-                }
-            }
-        }
-    
-        if (draw == true) {
-            return "Draw"
-        } else {
-            if (winner == "computer") {
-                ++computerScore
-                return "You lost"
-            } else if (winner == "player") {
-                ++playerScore
-                return "You win!!"
-            }
-        }
-    }
-
-    console.log(playRound())
-    console.log(playRound())
-    console.log(playRound())
-    console.log(playRound())
-    console.log(playRound())
-}
+};
 
 
 
-playGame()
+// Event listeners
+allOptionsContainer.addEventListener("click", function(event) {
+    const button = event.target;
+    assignPlayerChoice(button);
+});
+
+playButton.addEventListener("click", () => console.log("Hello World"));
